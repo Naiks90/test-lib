9b53caf6bbc7b4b3b58f5f35305f0b5c78406952
sasha.naiko <sasha.naiko@gmail.com>
2024-10-31 15:17:39 +0200

    add tailwind

--- src/components/PersonalButton.vue
+++ src/components/PersonalButton.vue
@@ -10,12 +10,13 @@ const props = defineProps({
 })
 </script>
 
-<style scoped>
+<style>
 .personal-button {
   background-color: #000;
   color: #fff;
   padding: 10px 20px;
   border: none;
   border-radius: 5px;
+  @apply bg-cyan-500;
 }
 </style>
---
+++ tailwind.config.js
@@ -0,0 +1,56 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  theme: {
+    theme: {
+      extend: {
+        transitionProperty: {
+          'max-height': 'max-height',
+          'transform': 'transform',
+          'opacity': 'opacity',
+          'left': 'left',
+        },
+        colors: {
+          'white': '#ffffff',
+          'gray': {
+            200: '#D9D9D9',
+            300: '#9D9D9D',
+            400: '#686868',
+            500: '#57534E',
+            550: '#727272',
+            600: '#403F40',
+            625: '#837A7A',
+            650: '#959595',
+            700: '#282627',
+            800: '#1E1C1D',
+            900: '#100E0F',
+            950: '#2F2E2F'
+          },
+          'cyan': {
+            350: '#70e6ff',
+            500: '#00D2FF',
+            700: '#155B75',
+            900: '#001923'
+          },
+          'red': {
+            500: '#FF4D4D',
+            600: '#D92D20',
+            800: '#912018',
+            900: '#742621'
+          },
+          'yellow': {
+            400: '#FFBE5B',
+            500: '#F79009',
+            600: '#DC6803',
+            800: '#93370D',
+          },
+          'green': {
+            700: '#027A48',
+            800: '#05603a'
+          }
+        }
+      },
+    },
+  },
+  plugins: [],
+}
